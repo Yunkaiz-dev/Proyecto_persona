2d3a3660ee6383747bac669991ced9aa2301a675
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2024-08-14 17:06:02 -0400

    Creacion de los DTO de persona

---
+++ DTO/CrearPersonaDTO.cs
@@ -0,0 +1,35 @@
+﻿namespace Proyecto_persona.DTO
+{
+    public class CrearPersonaDTO
+    {
+        public string PrimerNombre { get; set; } = null!;
+
+        public string? SegundoNombre { get; set; }
+
+        public string PrimerApellido { get; set; } = null!;
+
+        public string? SegundoApellido { get; set; }
+
+        public int Cedula { get; set; }
+
+        public int AñoNacimiento { get; set; }
+
+        public int MesNacimiento { get; set; }
+
+        public int DiaNacimiento { get; set; }
+
+        public int CodigoAreaNumero { get; set; }
+
+        public int CodigoPaisNumero { get; set; }
+
+        public int? CasaNumeroDireccion { get; set; }
+
+        public string? CalleDireccion { get; set; }
+
+        public string? CiudadDireccion { get; set; }
+
+        public string? ProvinciaDireccion { get; set; }
+
+        public string? PaisDireccion { get; set; }
+    }
+}
