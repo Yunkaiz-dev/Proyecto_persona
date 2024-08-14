e22f59391c1370dcf7799f23d9db829b3cfa96e7
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2024-08-14 17:04:54 -0400

    Realizar la abstraccion para las personas

---
+++ Abstraccion/Repositorios/IRepositorioPersona.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+using Proyecto_persona.DTO;
+using Proyecto_persona.Models;
+
+namespace Proyecto_persona.Abstraccion.Repositorios
+{
+    
+    public interface IRepositorioPersona
+    {
+        List<PersonaDTO> Get();
+        
+        PersonaDTO GetById(int id);
+        
+        PersonaDTO Create(CrearPersonaDTO crearPersonaDTO);
+  
+        PersonaDTO Update(ActualizarPersonaDTO actualizarPersonaDTO);
+
+        void Delete(int id);
+        
+
+        
+    }
+}
---
+++ Abstraccion/Servicios/IServicioPersona.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Proyecto_persona.Abstraccion.Servicios
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IServicioPersona : ControllerBase
+    {
+
+    }
+}
