8f1d0449d289d7558a5bf0fb4f35a9cda4d0893d
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2024-08-14 14:19:47 -0400

    Se agrego el controller para persona

---
+++ Controllers/PersonaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
+
+namespace Proyecto_persona.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonaController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Create()
+        {
+            string result = "Estoy retornando un Post";
+            return Ok(result);
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            string result = "Estoy retornando un Get";
+            return Ok(result);
+        }
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            string result = "Estoy retornando un Delete";
+            return Ok(result);
+        }
+        [HttpPatch]
+        public IActionResult Update()
+        {
+            string result = "Estoy retornando un Patch";
+            return Ok(result);
+        }
+    }
+}
