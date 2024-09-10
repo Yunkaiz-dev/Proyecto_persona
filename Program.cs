034a213c4aa0bf490e66a2b58573edb58432eb5b
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2024-09-10 01:25:04 -0400

    Clase program que ejecuta la API

--- Program.cs
+++ Program.cs
@@ -1,11 +1,38 @@
+using Microsoft.EntityFrameworkCore;
+using Proyecto_persona.Abstraccion.Repositorios;
+using Proyecto_persona.Abstraccion.Servicios;
+using Proyecto_persona.Implementaciones.Repositorios;
+using Proyecto_persona.Implementaciones.Servicios;
+using Proyecto_persona.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at httpsi://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<PersonaContext>(options =>
+            options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IRepositorioPersona, RepositorioPersona>();
+builder.Services.AddScoped<IServicioPersona, ServicioPersona>();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirTodo",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:5173")
+           .AllowAnyHeader()
+           .AllowAnyMethod()
+           .WithExposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Access-Control-Allow-Methods")
+           .SetIsOriginAllowed(origin => true)
+           .AllowCredentials();
+
+        });
+});
 
 var app = builder.Build();
 
@@ -16,9 +43,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
+
 app.UseHttpsRedirection();
 
+app.UseCors("PermitirTodo");
+
 app.UseAuthorization();
+
 
 app.MapControllers();
 
