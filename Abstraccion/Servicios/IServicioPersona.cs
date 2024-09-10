88a0e14726cf4b1b0ce2300b76d9465360fa60c4
Yunkaiz-dev <Vegarosariojoserafael@gmail.com>
2024-09-10 01:22:54 -0400

    Creacion de los servicios necesarios de la abstraccion para la API

--- Abstraccion/Servicios/IServicioPersona.cs
+++ Abstraccion/Servicios/IServicioPersona.cs
@@ -1,12 +1,22 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Proyecto_persona.DTO;
+using Proyecto_persona.Models;
 
 namespace Proyecto_persona.Abstraccion.Servicios
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class IServicioPersona : ControllerBase
+    
+    public interface IServicioPersona
     {
+        List<PersonaDTO> Get();
+
+        Persona GetById(int id);
+
+        PersonaDTO Create(CrearPersonaDTO crearPersonaDTO);
+
+        PersonaDTO Update(int id, ActualizarPersonaDTO actualizarPersonaDTO);
+
+        void Delete(int id);
 
     }
 }
